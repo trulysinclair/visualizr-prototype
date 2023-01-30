3fd2545c9295d7dc2c1b85942d1e6404d6446943
xsmith <silencegrim@gmail.com>
2023-01-30 18:34:52 -0500

    fix: "custom" type must be "table"

--- src/components/drop-tool.tsx
+++ src/components/drop-tool.tsx
@@ -3,9 +3,10 @@ import { DragEvent } from "react";
 
 export type DropToolProps = {
   icon: React.ReactNode;
+  type: string;
 };
 
-export function DropTool({ icon }: DropToolProps) {
+export function DropTool({ icon,type }: DropToolProps) {
   const onDragStart = (event: DragEvent<HTMLDivElement>, nodeType: string) => {
     event.dataTransfer.setData("application/reactflow", nodeType);
     event.dataTransfer.effectAllowed = "move";
@@ -14,7 +15,7 @@ export function DropTool({ icon }: DropToolProps) {
   return (
     <div
       className="flex h-10 w-10 items-center justify-center rounded bg-input-background/50 p-2 text-gray-300 shadow duration-150 hover:cursor-pointer hover:bg-input-background hover:text-white"
-      onDragStart={(event) => onDragStart(event, "custom")}
+      onDragStart={(event) => onDragStart(event, type)}
       draggable
     >
       {icon}
--- src/components/ui/toolbar.tsx
+++ src/components/ui/toolbar.tsx
@@ -6,8 +6,8 @@ import { DropTool } from "../drop-tool";
 export const Toolbar = () => (
   <Panel position="top-left" className="!inset-y-0 !left-0 flex items-center">
     <div className="space-y-2 rounded-2xl bg-primary p-4 text-black shadow-md">
-      <DropTool icon={<TableCellsIcon className="h-6 w-6" />} />
-      <DropTool icon={<Squares2X2Icon className="h-6 w-6" />} />
+      <DropTool type="table" icon={<TableCellsIcon className="h-6 w-6" />} />
+      <DropTool type="table" icon={<Squares2X2Icon className="h-6 w-6" />} />
     </div>
   </Panel>
 );
