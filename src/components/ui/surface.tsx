789713868c2adb638c95c1b2423dc3d0aa164c2c
xsmith <silencegrim@gmail.com>
2023-01-30 20:11:58 -0500

    refactor: remove newNode and use createNode

--- src/components/ui/surface.tsx
+++ src/components/ui/surface.tsx
@@ -3,13 +3,14 @@ import CustomEdge from "@/components/ui/custom-edge";
 import { MarkerDefinition } from "@/components/ui/marker";
 import { SurfaceAppbar } from "@/components/ui/surface-appbar";
 import { Toolbar } from "@/components/ui/toolbar";
+import { createNode } from "@/util/createNode";
 import {
+  DragEvent,
   useCallback,
   useEffect,
   useMemo,
   useRef,
   useState,
-  DragEvent,
 } from "react";
 import ReactFlow, {
   addEdge,
@@ -25,9 +26,6 @@ import ReactFlow, {
   useEdgesState,
   useNodesState,
 } from "reactflow";
-import { v4 } from "uuid";
-import { PSQLCharacterType, PSQLNumericType } from "../../types/postgresql";
-import { TableNode } from "../../types/table";
 
 const Surface = () => {
   const reactFlowWrapper = useRef<HTMLDivElement | null>(null);
@@ -42,7 +40,7 @@ const Surface = () => {
   const [snapToGrid, setSnapToGrid] = useState(false);
 
   const strokeColor = "stroke-accent-orange";
-  
+
   /**
    * When the graph is initialized, set the nodes and edges.
    */
@@ -76,7 +74,6 @@ const Surface = () => {
         )
       );
     },
-
     [setEdges]
   );
 
@@ -109,57 +106,7 @@ const Surface = () => {
         y: event.clientY - reactFlowBounds!.top,
       });
 
-      const newNode: TableNode = {
-        id: v4(),
-        type,
-        position: position!,
-        data: {
-          title: `New Table`,
-          description: "description",
-          columns: [
-            {
-              name: "id",
-              type: PSQLNumericType.SERIAL,
-              isPrimaryKey: true,
-
-              isUnique: false,
-              isNullable: false,
-              isAutoIncrement: true,
-              foreignKey: { table: "", column: "" },
-              length: 0,
-              defaultValue: "",
-              description: "",
-            },
-            {
-              name: "name",
-              type: PSQLCharacterType.TEXT,
-              isPrimaryKey: true,
-
-              isUnique: false,
-              isNullable: false,
-              isAutoIncrement: true,
-              foreignKey: null,
-              length: 0,
-              defaultValue: "",
-              description: "",
-            },
-            {
-              name: "id",
-              type: PSQLCharacterType.TEXT,
-              isPrimaryKey: true,
-              isUnique: false,
-              isNullable: false,
-              isAutoIncrement: true,
-              foreignKey: null,
-              length: 0,
-              defaultValue: "",
-              description: "",
-            },
-          ],
-        },
-      };
-
-      setNodes((nds) => nds.concat(newNode));
+      setNodes((nodes) => nodes.concat(createNode(type, position!)));
     },
     [reactflowInstance, setNodes]
   );
@@ -196,62 +143,64 @@ const Surface = () => {
     document.addEventListener("keyup", onKeyUp);
   }, [reactflowInstance]);
 
-  return ( <main className="h-screen w-full bg-black">
-  <ReactFlowProvider>
-    <div
-      className="relative h-full w-full overflow-hidden rounded-3xl bg-white"
-      ref={reactFlowWrapper}
-    >
-      <SurfaceAppbar />
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        deleteKeyCode={"Delete"}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        nodeTypes={nodeTypes}
-        edgeTypes={edgeTypes}
-        onConnect={onConnect}
-        onInit={onInit}
-        onDrop={onDrop}
-        snapGrid={[10, 10]}
-        snapToGrid={snapToGrid}
-        onDragOver={onDragOver}
-        fitView
-      >
-        <Controls
-          position="bottom-right"
-          className="z-20 overflow-hidden rounded border-b-secondary bg-input-background"
-        />
-        <MarkerDefinition id="start-one" color={strokeColor} />
-        <MarkerDefinition id="start-many" color={strokeColor} />
-        <MarkerDefinition id="start-zero-or-one" color={strokeColor} />
-        <MarkerDefinition id="start-one-or-many" color={strokeColor} />
-        <MarkerDefinition id="start-zero-or-many" color={strokeColor} />
-        <MarkerDefinition id="end-one" color={strokeColor} />
-        <MarkerDefinition id="end-many" color={strokeColor} />
-        <MarkerDefinition id="end-zero-or-one" color={strokeColor} />
-        <MarkerDefinition id="end-one-or-many" color={strokeColor} />
-        <MarkerDefinition id="end-zero-or-many" color={strokeColor} />
-        <MiniMap position="bottom-left" className="z-20" />
-        <Toolbar />
-        <Panel
-          position="top-right"
-          className="!inset-y-0 !right-0 flex items-center"
+  return (
+    <main className="h-screen w-full bg-black">
+      <ReactFlowProvider>
+        <div
+          className="relative h-full w-full overflow-hidden rounded-3xl bg-white"
+          ref={reactFlowWrapper}
         >
-          <div className="space-y-2 rounded-2xl bg-primary p-4 text-white shadow-md">
-            Sidebar
-          </div>
-        </Panel>
-        <Background
-          color="#374151"
-          className="bg-gray-800 fill-white"
-          variant={BackgroundVariant.Lines}
-        />
-      </ReactFlow>
-    </div>
-  </ReactFlowProvider>
-</main> );
-}
- 
+          <SurfaceAppbar />
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            deleteKeyCode={"Delete"}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            nodeTypes={nodeTypes}
+            edgeTypes={edgeTypes}
+            onConnect={onConnect}
+            onInit={onInit}
+            onDrop={onDrop}
+            snapGrid={[10, 10]}
+            snapToGrid={snapToGrid}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Controls
+              position="bottom-right"
+              className="z-20 overflow-hidden rounded border-b-secondary bg-input-background"
+            />
+            <MarkerDefinition id="start-one" color={strokeColor} />
+            <MarkerDefinition id="start-many" color={strokeColor} />
+            <MarkerDefinition id="start-zero-or-one" color={strokeColor} />
+            <MarkerDefinition id="start-one-or-many" color={strokeColor} />
+            <MarkerDefinition id="start-zero-or-many" color={strokeColor} />
+            <MarkerDefinition id="end-one" color={strokeColor} />
+            <MarkerDefinition id="end-many" color={strokeColor} />
+            <MarkerDefinition id="end-zero-or-one" color={strokeColor} />
+            <MarkerDefinition id="end-one-or-many" color={strokeColor} />
+            <MarkerDefinition id="end-zero-or-many" color={strokeColor} />
+            <MiniMap position="bottom-left" className="z-20" />
+            <Toolbar />
+            <Panel
+              position="top-right"
+              className="!inset-y-0 !right-0 flex items-center"
+            >
+              <div className="space-y-2 rounded-2xl bg-primary p-4 text-white shadow-md">
+                Sidebar
+              </div>
+            </Panel>
+            <Background
+              color="#374151"
+              className="bg-gray-800 fill-white"
+              variant={BackgroundVariant.Lines}
+            />
+          </ReactFlow>
+        </div>
+      </ReactFlowProvider>
+    </main>
+  );
+};
+
 export default Surface;
