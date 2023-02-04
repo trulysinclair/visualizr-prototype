38fe5896acfb9b74375c955915800b387798334f
xsmith <silencegrim@gmail.com>
2023-02-04 05:09:50 -0500

    feat: support psql relationships

--- src/util/refine-to-sql.ts
+++ src/util/refine-to-sql.ts
@@ -7,20 +7,46 @@ function refineToYaml<NodeData, EdgeData>(
 ): string {
   console.log(JSON.stringify(data, null, 2));
 
+  const getNode = (nodeId: string) =>
+    data.nodes.find((node) => node.id === nodeId) as TableNode;
+
   let sql = "";
   // @ts-ignore:disable-next-line
   data.nodes.forEach((node: TableNode) => {
     sql += `CREATE TABLE ${node.data.title} (\n`;
 
     node.data.columns.forEach((column) => {
-      sql += `  ${column.name} ${column.type} ${
-        column.isNullable ? "NULL" : "NOT NULL"
-      } ${column.isPrimaryKey ? "PRIMARY KEY" : ""} ${
-        column.isUnique ? "UNIQUE" : ""
-      }},\n`;
+      let field = [
+        "  ",
+        column.name,
+        column.type,
+        column.isNullable ? "NULL" : "NOT NULL",
+        column.isPrimaryKey ? "PRIMARY KEY" : null,
+        column.isUnique ? "UNIQUE" : null,
+      ]
+        .filter((item) => item != null)
+        .join(" ");
+
+      field += `,\n`;
+
+      sql += field;
     });
 
     sql += `);\n\n`;
+  });
+
+  data.edges.forEach((edge) => {
+    sql += `ALTER TABLE "${
+      getNode(edge.source)?.data.title
+    }" ADD FOREIGN KEY (${
+      getNode(edge.source).data.columns.find(
+        (column) => column.id === edge.sourceHandle?.replace("-source", "")
+      )?.name
+    }) REFERENCES ${getNode(edge.target).data.title} (${
+      getNode(edge.target).data.columns.find(
+        (column) => column.id === edge.targetHandle?.replace("-target", "")
+      )?.name
+    });\n`;
   });
 
   console.log(sql);
